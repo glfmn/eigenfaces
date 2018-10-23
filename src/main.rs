a578afcb0b22b342e9912a258519ce07dd9ef0fe
Gwen Lofman <Gwen@Lofman.io>
2018-10-22 20:06:35 -0400

    Load images into PCA and calculate distance matrix
    Load all of the gallery and testing images into the PCA in order
    to find the best match between the sets of images.

--- src/main.rs
+++ src/main.rs
@@ -101,11 +101,8 @@ fn sort_by_eigenvalue(values: &mut na::DVector<f64>, vectors: &mut na::DMatrix<f
     }
 }
 
-fn image_pca(basis_size: usize, data_set: &DataSet) -> DataSet {
-    let size = data_set.ncols();
-    if basis_size >= size {
-        panic!("Basis size larger than number of images in data set");
-    }
+/// Get the principal components of a data_set
+fn image_eigen(data_set: &DataSet) -> (DataSet, na::DVector<f64>) {
 
     let covariance = data_set.transpose() * data_set.clone();
     let na::SymmetricEigen {
@@ -116,7 +113,125 @@ fn image_pca(basis_size: usize, data_set: &DataSet) -> DataSet {
 
     let eigen = data_set.clone() * eigenvectors;
 
-    eigen.remove_columns(basis_size - 1, size as usize - basis_size)
+    (eigen, eigenvalues)
+}
+
+fn image_pca(basis_size: usize, basis: DataSet) -> DataSet {
+    let size = basis.ncols();
+    if basis_size >= size {
+        panic!("Basis size larger than number of images in data set");
+    }
+
+    basis.remove_columns(basis_size - 1, size as usize - basis_size)
+}
+
+fn calculate_image<R, C, SR, SC>(
+    image: &na::MatrixSlice<f64, R, C, SR, SC>,
+    min: f64,
+    max: f64,
+) -> Vec<u8>
+where
+    SR: na::Dim,
+    SC: na::Dim,
+    C: na::Dim,
+    R: na::Dim,
+{
+    image
+        .iter()
+        .map(|p| ((*p - min) / (max - min) * 255f64) as u8)
+        .collect()
+}
+
+fn write_image<P>(path: P, buffer: &Vec<u8>, w: u32, h: u32) -> std::io::Result<()>
+where
+    P: AsRef<std::ffi::OsStr>,
+{
+    use image::{jpeg, ColorType};
+    let path = std::path::Path::new(&path);
+    let mut file = File::create(path).unwrap();
+    if path.exists() {
+        println!(" overwrite {}", path.to_string_lossy());
+    } else {
+        println!(" create {}", path.to_string_lossy());
+    }
+    jpeg::JPEGEncoder::new(&mut file).encode(buffer.as_slice(), w, h, ColorType::Gray(8))?;
+
+    Ok(())
+}
+
+fn pca_load<S>(
+    image: &na::Matrix<f64, na::Dynamic, na::U1, S>,
+    pca: &DataSet,
+) -> na::DVector<f64>
+where
+    S: na::storage::Storage<f64, na::Dynamic, na::U1>,
+{
+    let mut pca_load = na::DVector::<f64>::from_element(pca.ncols(), 0.);
+
+    for c in 0..pca.ncols() {
+        let pc = pca.column(c);
+        pca_load[c] = (pc.transpose() * image)[0];
+    }
+
+    pca_load
+}
+
+fn pca_project(
+    pca_loading: &na::DVector<f64>,
+    pca: &DataSet,
+) -> ImageVector
+{
+    let mut projected = ImageVector::from_element(pca.nrows(), 0.);
+    for c in 0..pca.ncols() {
+        let pc = pca.column(c);
+        projected += pca_loading[c] * pc;
+    }
+
+    projected
+}
+
+struct Mahalanobis<'a> {
+    eigenvalues: &'a na::DVector<f64>,
+}
+
+impl<'a> Mahalanobis<'a> {
+    // Try to calculate the inverse of the covariance matrix and store it as the basis for
+    /// Create a mahalanobis distance calculator if the covariance matrix has an inverse
+    pub fn new(eigenvalues: &'a na::DVector<f64>) -> Self {
+        // Mahalanobis distance
+        Mahalanobis {
+            eigenvalues
+        }
+    }
+
+    /// Claculate the malahanobis distance for two vectors
+    pub fn distance<S>(
+        &self,
+        x: &na::Matrix<f64, na::Dynamic, na::U1, S>,
+        y: &na::Matrix<f64, na::Dynamic, na::U1, S>,
+    ) -> f64
+    where
+        S: na::storage::Storage<f64, na::Dynamic, na::U1>,
+     {
+        let difference = x - y;
+
+        difference.iter().enumerate().fold(0., |acc, (i, d)| {
+            acc + (1./self.eigenvalues[i]) * d * d
+        }).sqrt()
+    }
+
+    /// Calculate Mahalanobis distance for each column vector in each matrix
+    pub fn distance_matrix(&self, x: &na::DMatrix<f64>, y: &na::DMatrix<f64>) -> na::DMatrix<f64> {
+        let mut distance = na::DMatrix::<f64>::from_element(x.ncols(), y.ncols(), 0.);
+
+        for i in 0..x.ncols() {
+            for j in 0..y.ncols() {
+                distance[(i, j)] = self.distance(&x.column(i), &y.column(j));
+            }
+        }
+
+        distance
+    }
 }
 
 fn main() {
@@ -128,6 +243,8 @@ fn main() {
         (@arg OUTPUT: -o --output +takes_value "Path to place output files")
     ).get_matches();
 
+    let output = "output";
+
     // Load data-sets
     let (mut training_set, (w, h)) = match read_dataset("orl_faces", 1..=40, 3..=6) {
         Ok(data) => data,
@@ -136,30 +253,56 @@ fn main() {
             std::process::exit(1)
         }
     };
-    let (mut testing_set, _) = read_dataset("orl_faces", 1..=40, 1..=2).unwrap();
-    let (mut gallery_set, _) = read_dataset("orl_faces", 1..=40, 7..=10).unwrap();
+    let (testing_set, _) = read_dataset("orl_faces", 1..=40, 1..=2).unwrap();
+    let (gallery_set, _) = read_dataset("orl_faces", 1..=40, 7..=10).unwrap();
 
     let mean = mean_image(&training_set);
 
-    mean_center(&mean, &mut training_set);
-    mean_center(&mean, &mut testing_set);
-    mean_center(&mean, &mut gallery_set);
-
-    let eigen = image_pca(50, &training_set);
-    for i in 0..10 {
-        use image::{jpeg, ColorType};
-        let mut file = File::create(format!("output/eigenfaces/{}.png", i)).unwrap();
-        let (min, max) = eigen.iter().fold((std::f64::MAX, std::f64::MIN), |b, p| {
-            (b.0.min(*p), b.1.max(*p))
-        });
-        let buffer: Vec<u8> = eigen
-            .column(i)
-            .iter()
-            .map(|p| ((*p - min) / (max - min) * 255f64) as u8)
-            .collect();
-        jpeg::JPEGEncoder::new(&mut file)
-            .encode(buffer.as_slice(), w, h, ColorType::Gray(8))
+    {
+        let buffer: Vec<u8> = mean.iter().map(|p| *p as u8).collect();
+        write_image(format!("{}/mean_image.png", output), &buffer, w, h)
             .expect("failed to encode image");
-        println!("Creating image output/eigenfaces/{}.png", i + 1);
     }
+
+    mean_center(&mean, &mut training_set);
+
+    let (eigen, eigenvalues) = image_eigen(&training_set);
+    let pca = image_pca(50, eigen.clone());
+
+    let (min, max) = pca.iter().fold((std::f64::MAX, std::f64::MIN), |b, p| {
+        (b.0.min(*p), b.1.max(*p))
+    });
+
+    // Create and write images for eigenfaces
+
+    // 10 most significant eigenfaces
+    for i in 0..10 {
+        let buffer: Vec<u8> = calculate_image(&pca.column(i), min, max);
+        write_image(format!("{}/eigenfaces/{}.png", output, i + 1), &buffer, w, h)
+            .expect("failed to encode image");
+    }
+
+    // 10 least significant eigenfaces
+    for i in 39..50 {
+        let buffer: Vec<u8> = calculate_image(&pca.column(i), min, max);
+        write_image(format!("{}/eigenfaces/{}.png", output, i + 1), &buffer, w, h)
+            .expect("failed to encode image");
+    }
+
+    let mahalanobis = Mahalanobis::new(&eigenvalues);
+
+    let mut testing_pca_load: Vec<na::DVector<f64>> = vec![];
+    for i in 0..testing_set.ncols() {
+        testing_pca_load.push(pca_load(&testing_set.column(i), &pca));
+    }
+    let testing_pca_load: DataSet = DataSet::from_columns(testing_pca_load.as_slice());
+
+
+    let mut gallery_pca_load: Vec<na::DVector<f64>> = vec![];
+    for i in 0..gallery_set.ncols() {
+        gallery_pca_load.push(pca_load(&gallery_set.column(i), &pca));
+    }
+    let gallery_pca_load: DataSet = DataSet::from_columns(gallery_pca_load.as_slice());
+
+    let dist = mahalanobis.distance_matrix(&testing_pca_load, &gallery_pca_load);
 }
